dd6b6cdfc350fbb1ea9cadef062e68f62f6cfc91
moeyehtet96 <moeyehtet96@gmail.com>
2018-03-24 01:19:08 -0400

    Some changes to be able to compile with gcc

--- FaceTrackingTest/FaceTrackingTest/test.cpp
+++ FaceTrackingTest/FaceTrackingTest/test.cpp
@@ -1,7 +1,7 @@
-#include "opencv2\core.hpp"
-#include "opencv2\highgui.hpp"
-#include "opencv2\objdetect.hpp"
-#include "opencv2\imgproc.hpp"
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/imgproc.hpp>
 #include <iostream>
 #include <stdio.h>
 #include <cmath>
