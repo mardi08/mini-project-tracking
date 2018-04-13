5e3187608a3a2c171db3663212228c048db53476
mardi08 <mardi@purdue.edu>
2018-04-12 23:01:25 -0400

    adjustment

---
+++ FaceTrackingTest/FaceTrackingTest/test2/CMakeFiles/FaceTracking.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FaceTracking.dir/test.cpp.o"
+  "FaceTracking.pdb"
+  "FaceTracking"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FaceTracking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ FaceTrackingTest/FaceTrackingTest/test2/CMakeFiles/FaceTracking.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for FaceTracking.
+# This may be replaced when dependencies are built.
